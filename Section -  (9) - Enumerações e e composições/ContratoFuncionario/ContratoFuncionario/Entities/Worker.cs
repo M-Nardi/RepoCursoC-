bd5876dd7dfdeaad06ee7041c505a456e2556594
M-nardi <marcellonardi09@gmail.com>
2021-01-04 16:51:07 -0300

    Subindo Section 9 do curso

---
+++ Section -  (9) - Enumerações e e composições/ContratoFuncionario/ContratoFuncionario/Entities/Worker.cs
@@ -0,0 +1,38 @@
+﻿using ContratoFuncionario.Entities.Enums;
+using System.Collections.Generic;
+
+namespace ContratoFuncionario.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+        
+        
+        public Worker() 
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+        }
+
+        public void AddContract (HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract (HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+
+    }
+}
