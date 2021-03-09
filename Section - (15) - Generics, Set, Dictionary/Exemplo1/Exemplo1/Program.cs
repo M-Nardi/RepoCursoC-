be3ccc0ac0c4824a0b9738535daaa5d25d3091cf
M-nardi <marcellonardi09@gmail.com>
2021-03-09 15:26:36 -0300

    Generics

---
+++ Section - (15) - Generics, Set, Dictionary/Exemplo1/Exemplo1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using Exemplo1.Services;
+
+namespace Exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+
+            Console.Write("How many values? : ");
+            int amountValues = int.Parse(Console.ReadLine());
+
+            PrintService list = new PrintService();
+
+            while (cont < amountValues)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                list.AddValue(number);
+
+                cont++;
+            }
+
+            list.Print();
+            Console.WriteLine("First: " + list.First());
+
+        }
+    }
+}
---
+++ Section - (15) - Generics, Set, Dictionary/Exemplo1/Exemplo1/Services/PrintService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exemplo1.Services
+{
+    class PrintService
+    {
+
+        private int[] _values = new int[10];
+        private int _count = 0;
+
+        public void AddValue (int value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("Numeros máximos atingidos");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public int First()
+        {
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i=0; i < _count-1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+
+        }
+
+    }
+}
